db5f18310cc6da929bf89ca6dea6872644c51594
HieuTVGCH200824 <hieutvgch200824@fpt.edu.vn>
2022-08-18 00:41:30 +0700

    User update

---
+++ Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Assignment.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Assignment.Controllers
+{
+    public class UserController : Controller
+    {
+        private ApplicationDbContext context;
+        public UserController(ApplicationDbContext applicationDbContext)
+        {
+            context = applicationDbContext;
+        }
+        public IActionResult Index()
+        {
+            var user = context.Users.ToList();
+            var userRoles = context.UserRoles.ToList();
+            ViewBag.Roles = userRoles;
+            return View(user);
+        }
+    }
+}
