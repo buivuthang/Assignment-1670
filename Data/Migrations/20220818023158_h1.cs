112fe570f5231828277191ff0a30d874cc7367d8
HieuTVGCH200824 <hieutvgch200824@fpt.edu.vn>
2022-08-18 09:38:23 +0700

    Search fix

---
+++ Data/Migrations/20220818023158_h1.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Assignment.Data.Migrations
+{
+    public partial class h1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1",
+                column: "ConcurrencyStamp",
+                value: "fc13b203-9b09-4895-9fae-ad7d133469c9");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2",
+                column: "ConcurrencyStamp",
+                value: "86c6abb1-e83b-4a90-9dbb-58721ff9466a");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3",
+                column: "ConcurrencyStamp",
+                value: "0e0a4372-5caa-4eb5-8e03-4c4d5d8096df");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "d6482ad7-40a1-4f13-bc3b-92b9d6ca52ba", "AQAAAAEAACcQAAAAECkbc+0A8RSPruPMY5fvGDviyWblyRhLNI60vyTf7VUZaHzsVmHkHs+meEUPzyC6Sg==", "d4ae915b-60f6-48f9-a2f9-144844f5464e" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "2",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "985fe068-4c9c-42de-8afe-b39a02248e03", "AQAAAAEAACcQAAAAEMDpJG6ic/sK7Cm1YTAc932mn94XaMFWax95v2sACduCfTWotmPJoRcV2//ER/kgoQ==", "2523cc5c-1dc5-44da-93bb-e3af97651a46" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "3",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "2406813a-c51b-47ec-8469-7f785fa3508d", "AQAAAAEAACcQAAAAEIYXUgbCSXH2+upRAuU+VTp9rKQsdzx9Snhqbf1Aqh36KHZdLQnS3SlhicFuNMMOSw==", "75c6560f-6ade-4220-a271-9b3b88f68c79" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1",
+                column: "ConcurrencyStamp",
+                value: "f0748963-3c9e-466a-9180-8f0cc3adce45");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2",
+                column: "ConcurrencyStamp",
+                value: "1efd9d1c-d733-458c-9647-59eeaaf5ac7b");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3",
+                column: "ConcurrencyStamp",
+                value: "6af31bd2-8096-449d-8475-2f6bc7f417ed");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "dc8557ea-dd39-49b4-a8c8-e78c8fe163d7", "AQAAAAEAACcQAAAAEFERXBPeYegQwIKhTnylfQbEuuJR7U9x6QESSj9YmNqX9eoRUtSaEAq9UWEW2JDDoA==", "e64ae0d0-e216-4e7b-8285-c26224adb6f9" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "2",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "cfaf894d-34c4-45ff-889e-dbaab350183a", "AQAAAAEAACcQAAAAEINUlOui79uB9oxi/vkfjrVf7/3iXZQXP/ostdvHbzI/OA/an0UwTc41nTtJOxT0UA==", "9c943c90-3b70-4cae-a27a-1478ea7ab495" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "3",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "639b382e-3f24-41ac-9683-fd4ac072bc3e", "AQAAAAEAACcQAAAAELqHOcArtxdSESE8G426aPKFsBVsRS/GY/PskuAdTBmkU5pvdeOzAYKnTHxPod5vcQ==", "42f2bbe2-180c-4143-abf1-013b547b1b11" });
+        }
+    }
+}
--- obj/Debug/netcoreapp3.1/Razor/Views/Cart/Cart.cshtml.g.cs
+++ obj/Debug/netcoreapp3.1/Razor/Views/Cart/Cart.cshtml.g.cs
@@ -13,14 +13,14 @@ namespace AspNetCore
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
 #nullable restore
-#line 1 "E:\GCH0904\1670\Asm\Assignment-1670\Views\_ViewImports.cshtml"
+#line 1 "C:\Users\hieupc\source\repos\Assignment-1670\Views\_ViewImports.cshtml"
 using Assignment;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "E:\GCH0904\1670\Asm\Assignment-1670\Views\_ViewImports.cshtml"
+#line 2 "C:\Users\hieupc\source\repos\Assignment-1670\Views\_ViewImports.cshtml"
 using Assignment.Models;
 
 #line default
@@ -28,7 +28,9 @@ using Assignment.Models;
 #nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"db6a4e76911265ffa4db42121c053ace230f941f", @"/Views/Cart/Cart.cshtml")]
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"12290ae8b54fdba546f13d1190a2a2d1619f6758", @"/Views/_ViewImports.cshtml")]
+    #nullable restore
     public class Views_Cart_Cart : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<List<Assignment.Models.CartItem>>
+    #nullable disable
     {
         private static readonly global::Microsoft.AspNetCore.Razor.TagHelpers.TagHelperAttribute __tagHelperAttribute_0 = new global::Microsoft.AspNetCore.Razor.TagHelpers.TagHelperAttribute("min", new global::Microsoft.AspNetCore.Html.HtmlString("1"), global::Microsoft.AspNetCore.Razor.TagHelpers.HtmlAttributeValueStyle.DoubleQuotes);
         private static readonly global::Microsoft.AspNetCore.Razor.TagHelpers.TagHelperAttribute __tagHelperAttribute_1 = new global::Microsoft.AspNetCore.Razor.TagHelpers.TagHelperAttribute("asp-route", "removecart", global::Microsoft.AspNetCore.Razor.TagHelpers.HtmlAttributeValueStyle.DoubleQuotes);
@@ -64,7 +66,7 @@ using Assignment.Models;
         {
             WriteLiteral("\r\n<h2>Shopping Cart</h2>\r\n");
 #nullable restore
-#line 4 "E:\GCH0904\1670\Asm\Assignment-1670\Views\Cart\Cart.cshtml"
+#line 4 "C:\Users\hieupc\source\repos\Assignment-1670\Views\Cart\Cart.cshtml"
  if (@TempData["Message"] != null)
 {
 
@@ -73,7 +75,7 @@ using Assignment.Models;
 #nullable disable
             WriteLiteral("    <p class=\"alert alert-danger\">");
 #nullable restore
-#line 6 "E:\GCH0904\1670\Asm\Assignment-1670\Views\Cart\Cart.cshtml"
+#line 6 "C:\Users\hieupc\source\repos\Assignment-1670\Views\Cart\Cart.cshtml"
                              Write(TempData["Message"]);
 
 #line default
@@ -81,14 +83,14 @@ using Assignment.Models;
 #nullable disable
             WriteLiteral("</p>\r\n");
 #nullable restore
-#line 7 "E:\GCH0904\1670\Asm\Assignment-1670\Views\Cart\Cart.cshtml"
+#line 7 "C:\Users\hieupc\source\repos\Assignment-1670\Views\Cart\Cart.cshtml"
 }
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "E:\GCH0904\1670\Asm\Assignment-1670\Views\Cart\Cart.cshtml"
+#line 8 "C:\Users\hieupc\source\repos\Assignment-1670\Views\Cart\Cart.cshtml"
  if (Model.Count > 0)
 {
     double total = 0;
@@ -100,7 +102,7 @@ using Assignment.Models;
 #nullable disable
             WriteLiteral("    <table class=\"table\">\r\n        <tr>\r\n            <th>#</th>\r\n            <th>Book</th>\r\n            <th>Price</th>\r\n            <th>Quantity</th>\r\n            <th>Sub-Total</th>\r\n            <th></th>\r\n        </tr>\r\n");
 #nullable restore
-#line 22 "E:\GCH0904\1670\Asm\Assignment-1670\Views\Cart\Cart.cshtml"
+#line 22 "C:\Users\hieupc\source\repos\Assignment-1670\Views\Cart\Cart.cshtml"
          foreach (var cartitem in Model)
         {
             var amount = cartitem.quantity * cartitem.book.Price;
@@ -112,7 +114,7 @@ using Assignment.Models;
 #nullable disable
             WriteLiteral("            <tr>\r\n                <td>");
 #nullable restore
-#line 28 "E:\GCH0904\1670\Asm\Assignment-1670\Views\Cart\Cart.cshtml"
+#line 28 "C:\Users\hieupc\source\repos\Assignment-1670\Views\Cart\Cart.cshtml"
                 Write(number++);
 
 #line default
@@ -120,7 +122,7 @@ using Assignment.Models;
 #nullable disable
             WriteLiteral("</td>\r\n                <td>");
 #nullable restore
-#line 29 "E:\GCH0904\1670\Asm\Assignment-1670\Views\Cart\Cart.cshtml"
+#line 29 "C:\Users\hieupc\source\repos\Assignment-1670\Views\Cart\Cart.cshtml"
                Write(cartitem.book.Title);
 
 #line default
@@ -129,19 +131,19 @@ using Assignment.Models;
             WriteLiteral("</td>\r\n                <td>");
 #nullable restore
 #line 30 "C:\Users\hieupc\source\repos\Assignment-1670\Views\Cart\Cart.cshtml"
-                Write(cartitem.book.Price.ToString("n0"));
+               Write(cartitem.book.Price);
 
 #line default
 #line hidden
 #nullable disable
             WriteLiteral("</td>\r\n                <td>");
-            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("input", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.SelfClosing, "806e0977ba42407d3ad9964fb85f4825142242477421", async() => {
+            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("input", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.SelfClosing, "db6a4e76911265ffa4db42121c053ace230f941f7405", async() => {
             }
             );
             __Microsoft_AspNetCore_Mvc_TagHelpers_InputTagHelper = CreateTagHelper<global::Microsoft.AspNetCore.Mvc.TagHelpers.InputTagHelper>();
             __tagHelperExecutionContext.Add(__Microsoft_AspNetCore_Mvc_TagHelpers_InputTagHelper);
 #nullable restore
-#line 31 "E:\GCH0904\1670\Asm\Assignment-1670\Views\Cart\Cart.cshtml"
+#line 31 "C:\Users\hieupc\source\repos\Assignment-1670\Views\Cart\Cart.cshtml"
 __Microsoft_AspNetCore_Mvc_TagHelpers_InputTagHelper.For = ModelExpressionProvider.CreateModelExpression(ViewData, __model => cartitem.quantity);
 
 #line default
@@ -151,7 +153,7 @@ __Microsoft_AspNetCore_Mvc_TagHelpers_InputTagHelper.For = ModelExpressionProvid
             BeginAddHtmlAttributeValues(__tagHelperExecutionContext, "id", 1, global::Microsoft.AspNetCore.Razor.TagHelpers.HtmlAttributeValueStyle.DoubleQuotes);
 #nullable restore
 #line 31 "C:\Users\hieupc\source\repos\Assignment-1670\Views\Cart\Cart.cshtml"
-AddHtmlAttributeValue("", 841, $"quantity-{cartitem.book.Id}", 841, 33, false);
+AddHtmlAttributeValue("", 824, $"quantity-{cartitem.book.Id}", 824, 33, false);
 
 #line default
 #line hidden
@@ -161,7 +163,7 @@ AddHtmlAttributeValue("", 841, $"quantity-{cartitem.book.Id}", 841, 33, false);
             BeginAddHtmlAttributeValues(__tagHelperExecutionContext, "max", 1, global::Microsoft.AspNetCore.Razor.TagHelpers.HtmlAttributeValueStyle.DoubleQuotes);
 #nullable restore
 #line 31 "C:\Users\hieupc\source\repos\Assignment-1670\Views\Cart\Cart.cshtml"
-AddHtmlAttributeValue("", 889, cartitem.book.Quantity, 889, 23, false);
+AddHtmlAttributeValue("", 872, cartitem.book.Quantity, 872, 23, false);
 
 #line default
 #line hidden
@@ -177,21 +179,21 @@ AddHtmlAttributeValue("", 889, cartitem.book.Quantity, 889, 23, false);
             WriteLiteral("</td>\r\n                <td>");
 #nullable restore
 #line 32 "C:\Users\hieupc\source\repos\Assignment-1670\Views\Cart\Cart.cshtml"
-                Write(amount.ToString("n0"));
+               Write(amount);
 
 #line default
 #line hidden
 #nullable disable
             WriteLiteral("</td>\r\n                <td>\r\n                    <button class=\"btn btn-success updatecartitem\"\r\n                    data-bookid=\"");
 #nullable restore
-#line 35 "E:\GCH0904\1670\Asm\Assignment-1670\Views\Cart\Cart.cshtml"
+#line 35 "C:\Users\hieupc\source\repos\Assignment-1670\Views\Cart\Cart.cshtml"
                             Write(cartitem.book.Id);
 
 #line default
 #line hidden
 #nullable disable
             WriteLiteral("\">\r\n                        Update\r\n                    </button>\r\n                    ");
-            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("a", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "806e0977ba42407d3ad9964fb85f48251422424710562", async() => {
+            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("a", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "db6a4e76911265ffa4db42121c053ace230f941f10530", async() => {
                 WriteLiteral("Delete");
             }
             );
@@ -205,7 +207,7 @@ AddHtmlAttributeValue("", 889, cartitem.book.Quantity, 889, 23, false);
             }
             BeginWriteTagHelperAttribute();
 #nullable restore
-#line 38 "E:\GCH0904\1670\Asm\Assignment-1670\Views\Cart\Cart.cshtml"
+#line 38 "C:\Users\hieupc\source\repos\Assignment-1670\Views\Cart\Cart.cshtml"
                                                     WriteLiteral(cartitem.book.Id);
 
 #line default
@@ -224,7 +226,7 @@ AddHtmlAttributeValue("", 889, cartitem.book.Quantity, 889, 23, false);
             __tagHelperExecutionContext = __tagHelperScopeManager.End();
             WriteLiteral("\r\n                </td>\r\n            </tr>\r\n");
 #nullable restore
-#line 41 "E:\GCH0904\1670\Asm\Assignment-1670\Views\Cart\Cart.cshtml"
+#line 41 "C:\Users\hieupc\source\repos\Assignment-1670\Views\Cart\Cart.cshtml"
         }
 
 #line default
@@ -233,14 +235,14 @@ AddHtmlAttributeValue("", 889, cartitem.book.Quantity, 889, 23, false);
             WriteLiteral("        <tr>\r\n            <td colspan=\"4\" class=\"text-right\">Total Price</td>\r\n            <td>");
 #nullable restore
 #line 44 "C:\Users\hieupc\source\repos\Assignment-1670\Views\Cart\Cart.cshtml"
-            Write(total.ToString("n0"));
+           Write(total);
 
 #line default
 #line hidden
 #nullable disable
             WriteLiteral("</td>\r\n            <td></td>\r\n        </tr>\r\n    </table>\r\n");
             WriteLiteral("    ");
-            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("form", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "806e0977ba42407d3ad9964fb85f48251422424713484", async() => {
+            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("form", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "db6a4e76911265ffa4db42121c053ace230f941f13436", async() => {
                 WriteLiteral(@"
         <input type=""hidden"" name=""id"" />
         <input type=""hidden"" name=""id"" />
@@ -279,7 +281,7 @@ AddHtmlAttributeValue("", 889, cartitem.book.Quantity, 889, 23, false);
                     type: ""POST"",
                     url: """);
 #nullable restore
-#line 68 "E:\GCH0904\1670\Asm\Assignment-1670\Views\Cart\Cart.cshtml"
+#line 68 "C:\Users\hieupc\source\repos\Assignment-1670\Views\Cart\Cart.cshtml"
                      Write(Url.RouteUrl("updatecart"));
 
 #line default
@@ -287,7 +289,7 @@ AddHtmlAttributeValue("", 889, cartitem.book.Quantity, 889, 23, false);
 #nullable disable
                 WriteLiteral("\",\r\n                    data: {\r\n                        bookid: bookid,\r\n                        quantity: quantity\r\n                    },\r\n                    success: function(result) {\r\n                        window.location.href = \"");
 #nullable restore
-#line 74 "E:\GCH0904\1670\Asm\Assignment-1670\Views\Cart\Cart.cshtml"
+#line 74 "C:\Users\hieupc\source\repos\Assignment-1670\Views\Cart\Cart.cshtml"
                                            Write(Url.RouteUrl("cart"));
 
 #line default
@@ -297,7 +299,7 @@ AddHtmlAttributeValue("", 889, cartitem.book.Quantity, 889, 23, false);
             }
             );
 #nullable restore
-#line 80 "E:\GCH0904\1670\Asm\Assignment-1670\Views\Cart\Cart.cshtml"
+#line 80 "C:\Users\hieupc\source\repos\Assignment-1670\Views\Cart\Cart.cshtml"
      
 
 }
@@ -309,7 +311,7 @@ else
 #nullable disable
             WriteLiteral("    <p class=\"alert alert-danger\">No product added !</p>\r\n");
 #nullable restore
-#line 86 "E:\GCH0904\1670\Asm\Assignment-1670\Views\Cart\Cart.cshtml"
+#line 86 "C:\Users\hieupc\source\repos\Assignment-1670\Views\Cart\Cart.cshtml"
 }
 
 #line default
@@ -317,16 +319,26 @@ else
 #nullable disable
         }
         #pragma warning restore 1998
+        #nullable restore
         [global::Microsoft.AspNetCore.Mvc.Razor.Internal.RazorInjectAttribute]
-        public global::Microsoft.AspNetCore.Mvc.ViewFeatures.IModelExpressionProvider ModelExpressionProvider { get; private set; }
+        public global::Microsoft.AspNetCore.Mvc.ViewFeatures.IModelExpressionProvider ModelExpressionProvider { get; private set; } = default!;
+        #nullable disable
+        #nullable restore
         [global::Microsoft.AspNetCore.Mvc.Razor.Internal.RazorInjectAttribute]
-        public global::Microsoft.AspNetCore.Mvc.IUrlHelper Url { get; private set; }
+        public global::Microsoft.AspNetCore.Mvc.IUrlHelper Url { get; private set; } = default!;
+        #nullable disable
+        #nullable restore
         [global::Microsoft.AspNetCore.Mvc.Razor.Internal.RazorInjectAttribute]
-        public global::Microsoft.AspNetCore.Mvc.IViewComponentHelper Component { get; private set; }
+        public global::Microsoft.AspNetCore.Mvc.IViewComponentHelper Component { get; private set; } = default!;
+        #nullable disable
+        #nullable restore
         [global::Microsoft.AspNetCore.Mvc.Razor.Internal.RazorInjectAttribute]
-        public global::Microsoft.AspNetCore.Mvc.Rendering.IJsonHelper Json { get; private set; }
+        public global::Microsoft.AspNetCore.Mvc.Rendering.IJsonHelper Json { get; private set; } = default!;
+        #nullable disable
+        #nullable restore
         [global::Microsoft.AspNetCore.Mvc.Razor.Internal.RazorInjectAttribute]
-        public global::Microsoft.AspNetCore.Mvc.Rendering.IHtmlHelper<List<Assignment.Models.CartItem>> Html { get; private set; }
+        public global::Microsoft.AspNetCore.Mvc.Rendering.IHtmlHelper<List<Assignment.Models.CartItem>> Html { get; private set; } = default!;
+        #nullable disable
     }
 }
 #pragma warning restore 1591
